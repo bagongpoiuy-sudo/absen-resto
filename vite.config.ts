32ce955b1f26c5bfc79172dc923cb697f8a9b165
muhammad fazle ferino <m.fazle2006@gmail.com>
2026-05-30 19:35:14 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Sistem Absen QR Code',
+        short_name: 'AbsenQR',
+        description: 'Aplikasi Presensi Menggunakan Scan QR Code',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // Nama icon ukuran 192x192 di folder public
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png', // Nama icon ukuran 512x512 di folder public
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
